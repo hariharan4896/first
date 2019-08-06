d4c99eac4b7ade13ecc1db2244e489ca9aa30b1d
hariharan4896 <hariharan4896@gmail.com>
2019-08-06 03:52:51 -0700

    hello

---
+++ first.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a=3;
+	int b=6;
+	printf("a=%d,b=%d\n",%a,%b);
+	b=b+a;
+	a=b-a;
+	b=b-a;
+	printf("a=%d,b=%d\n",%a,%b);
+
+}
---
+++ second.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int a=3;
+	int b=6;
+	int temp;
+	printf("a=%d,b=%d\n",%a,%b);
+	temp =a;
+	a=b;
+	b=temp;
+	printf("a=%d,b=%d\n",%a,%b);
+}
